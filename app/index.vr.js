e2e10dccce4d0bad8b6e1fded86c62201fa938be
jsealt <jsealt@users.noreply.github.com>
2020-07-05 21:47:53 -0400

    droneshots
    removed santorini text, and added drone shots to sandbox

--- app/index.vr.js
+++ app/index.vr.js
@@ -1,35 +1,28 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React from "react";
+import { AppRegistry, asset, Pano, Text, View } from "react-vr";
 
 export default class app extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('santoriniSunset.jpg')}/>
+        <Pano source={asset("drone.jpg")} />
         <Text
           style={{
             // backgroundColor: '#777879',commented out
             fontSize: 0.8,
-            color: '#bf1363',
-            fontWeight: '300',
+            color: "#bf1363",
+            fontWeight: "300",
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          Santorini, Greece
-        </Text>
+            textAlign: "center",
+            textAlignVertical: "center",
+            transform: [{ translate: [0, 0, -3] }],
+          }}
+        ></Text>
       </View>
     );
   }
-};
+}
 
-AppRegistry.registerComponent('app', () => app);
+AppRegistry.registerComponent("app", () => app);
