4b2745c376c895a8447996fa59bd366a1ca56a90
jsealt <jsealt@users.noreply.github.com>
2020-07-11 21:14:02 -0400

    edits
    commented out background pane, changed color of text box, increased font size, added a second row of text to the welcome message, centered the text in the box by copying the align lines from the background. assigned the angell colorway. prepare for screen capture.

--- app-react360/index.js
+++ app-react360/index.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-360";
 
 export default class app_react360 extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
+          <Text style={styles.greeting}>"Gods Country" </Text>
+          <Text style={(styles.greeting, (fontSize = 24))}>
+            Lothian, MD - July 2020{" "}
           </Text>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // backgroundColor: "rgba(255, 255, 255, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
   },
   greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    padding: 40,
+    backgroundColor: "#623cea",
+    borderColor: "#639dda",
     borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center",
   },
   greeting: {
-    fontSize: 30,
+    fontSize: 80,
   },
 });
 
-AppRegistry.registerComponent('app_react360', () => app_react360);
+AppRegistry.registerComponent("app_react360", () => app_react360);
