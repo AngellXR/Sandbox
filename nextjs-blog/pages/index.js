0af158072e189171dcfcac7f8064a1e6d2e6ccb3
jsealt <jsealt@users.noreply.github.com>
2020-07-15 18:49:38 -0400

    altontech footer
    added at logo, experimented with images

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -10,21 +10,21 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://Instagram.com/projectangell">AngellXR!</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Collaborate using <code>#angellxr</code>
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
+            <h3>About &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
+            <h3>Prototypes &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -32,7 +32,7 @@ export default function Home() {
             href="https://github.com/zeit/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
+            <h3>API &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -40,7 +40,7 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Roadmap &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
@@ -49,13 +49,10 @@ export default function Home() {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        <a href="https://alton.tech/" target="_blank" rel="noopener noreferrer">
+          product of
+          <img src="/atlogo2.png" alt="AltonTech Logo" className="logo" />
+          AltonTech, Inc.
         </a>
       </footer>
 
